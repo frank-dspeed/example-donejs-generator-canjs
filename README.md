2d86f241b52fe4318da61af9d3d9798a7230021d
Frank Lemanschik <frank@dspeed.eu>
2017-06-24 13:53:08 +0100

    0.0.1

---
+++ README.md
@@ -0,0 +1,38 @@
+donejs-slim - Full-stack realtime
+
+[![Build Status](https://travis-ci.org/direktspeed/donejs-slim.svg?branch=master)](https://travis-ci.org/direktspeed/donejs-slim)
+[![npm version](https://badge.fury.io/js/donejs-slim.svg)](http://badge.fury.io/js/donejs-slim)
+
+
+A generator to quickly add StealJS Slim Loader to your DoneJS project.
+
+## Using the generator
+
+To add this generator to your DoneJS application run
+
+```
+donejs add slim
+```
+
+**This generator could potentially overwrite files in an existing application. It is meant to be used immediately after generating a new DoneJS application.**
+
+The following packages will be added to your project from npm:
+- `???`
+
+
+
+## Developing
+
+To make changes to this generator clone the repository and install the dependencies
+
+```
+git clone git@github.com:donejs/donejs-slim.git
+cd donejs-slim
+npm install
+```
+
+Then you can run the tests with
+
+```
+npm test
+```
---
+++ default/index.js
@@ -0,0 +1,111 @@
+var Generator = require('yeoman-generator');
+var path = require('path');
+var _ = require('lodash');
+var utils = require('../lib/utils');
+var cmd = require('child_process').execSync;
+
+var yarnInstalled;
+try {
+  cmd('yarn bin').toString();
+  yarnInstalled = true;
+} catch (err) {
+  yarnInstalled = false;
+}
+
+module.exports = class extends Generator {
+  // templatePath: utils.templatePath(path.join('.donejs', 'templates', 'app')),
+
+  constructor (args, opts) {
+    super(args, opts);
+
+    this.props = {};
+  }
+
+  prompting () {
+    var done = this.async();
+
+    console.log('');
+    console.log('Add StealJS Slim Loader to your DoneJS app.');
+    console.log('');
+
+    var prompts = [{
+      name: 'packager',
+      type: 'list',
+      message: 'Which package manager are you using (has to be installed globally)?',
+      default: yarnInstalled ? 'yarn@>= 0.18.0' : 'npm@>= 3.0.0',
+      choices: [{
+        name: 'npm',
+        value: 'npm@>= 3.0.0'
+      }, {
+        name: 'Yarn',
+        value: 'yarn@>= 0.18.0'
+      }]
+    }];
+
+    this.prompt(prompts).then(props => {
+      console.log(props);
+      this.props = props;
+      done();
+    });
+  }
+
+  writing () {
+    var done = this.async();
+    var pkg = utils.getPkgOrBail(this, done);
+    if (!pkg) {
+      return;
+    }
+
+/*
+    // Make a general options object for all pages.
+    var folder = _.get(pkg, 'system.directories.lib') || './src';
+    var options = {
+      pkgName: pkg.name
+    };
+    _.merge(options, this.props);
+
+
+    // copy app.js
+    this.fs.copyTpl(
+      this.templatePath('app.js'),
+      this.destinationPath(path.join(folder, 'app.js')),
+      options
+    );
+
+    const copyModelTemplate = (name, options) => {
+      this.fs.copyTpl(
+        this.templatePath(path.join('models', name)),
+        this.destinationPath(path.join(folder, 'models', name)),
+        options
+      );
+    };
+    const modelFiles = [
+      'algebra.js',
+      'behaviors.js',
+      'feathers-client.js',
+      'session.js',
+      'user.js',
+      'fixtures/fixtures.js'
+    ];
+    modelFiles.forEach(fileName => copyModelTemplate(fileName, options));
+
+    var newDependencies = [
+      'can-connect-feathers',
+      'feathers',
+      'feathers-socketio',
+      'feathers-hooks',
+      'feathers-authentication-client',
+      'socket.io-client',
+      'steal-socket.io'
+    ];
+    if (this.props.packager === 'yarn@>= 0.18.0') {
+      console.log('installing with yarn');
+      this.yarnInstall(newDependencies, {'save': true});
+    } else {
+      console.log('installing with npm');
+      this.npmInstall(newDependencies, {'save': true});
+    }
+*/
+    done();
+  }
+};
